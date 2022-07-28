03b71ba620664d1e21d52fb39353df5d882f5e84
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-07-28 22:00:05 +0600

    dental care area complete

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,7 +6,7 @@ import phone from '../../assets/icons/phone.svg';
 
 const Info = ({img, cardTitle, cardDetail}) => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-2'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
             <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" cardDetail="This is the first card" img={clock}></InfoCard>
             <InfoCard cardTitle="Our locations" bgClass="bg-accent" cardDetail="This is the second card" img={marker}></InfoCard>
             <InfoCard cardTitle="Contact US" bgClass="bg-gradient-to-r from-secondary to-primary" cardDetail="This is the third card" img={phone}></InfoCard>
--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -3,16 +3,13 @@ import Services from './Services';
 
 const Service = ({service}) => {
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
     <img src={service.img} alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
     <h2 class="card-title">{service.name}</h2>
     <p>{service.description}</p>
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
   </div>
 </div>
     );

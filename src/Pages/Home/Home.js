49c4345ccfce7452907af22e0bc44c28ef57a6ce
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-07-28 23:37:06 +0600

    Testimonials complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ import Care from './Care';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Services></Services>
             <Care></Care>
             <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
         </div>
     );
 };

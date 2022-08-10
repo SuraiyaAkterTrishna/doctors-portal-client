2f4c69b9bab32007814609ac1432e7701c0d7266
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-08-10 21:28:46 +0600

    Login card UI design

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is from login</h2>
+        <div className='flex h-screen justify-center items-center'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl font-bold">Login</h2>
+                    <div class="divider">OR</div>
+                    <button class="btn btn-outline">Continue With Google</button>
+                </div>
+            </div>
         </div>
     );
 };
